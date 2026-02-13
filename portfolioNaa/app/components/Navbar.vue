2d92856e1fe0d27130cf78ec1182f9037ea971e1
Moges741 <mogesse741@gmail.com>
2026-02-13 10:35:29 +0300

    added navbar

---
+++ portfolioNaa/app/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav class="fixed top-0 left-0 w-full z-50 backdrop-blur bg-black/70 border-b border-white/5">
+    <div class="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
+
+      <h1 class="text-2xl font-bold bg-gradient-to-r from-purple-500 to-indigo-500 bg-clip-text text-transparent">
+        MOGES
+      </h1>
+      <ul class="hidden md:flex gap-8 text-gray-300 font-medium">
+        <li class="hover:text-white transition cursor-pointer">Home</li>
+        <li class="hover:text-white transition cursor-pointer">About</li>
+        <li class="hover:text-white transition cursor-pointer">Services</li>
+        <li class="hover:text-white transition cursor-pointer">Tools</li>
+        <li class="hover:text-white transition cursor-pointer">Projects</li>
+        <li class="hover:text-white transition cursor-pointer">Contact</li>
+      </ul>
+
+    </div>
+  </nav>
+</template>
